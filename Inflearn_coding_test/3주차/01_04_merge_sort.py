588cf51674dccaecce0bcf760b84da59eab9cea0
MuSun Choi <129160983+moosunny@users.noreply.github.com>
2024-12-16 21:25:42 +0900

    Update 01_04_merge_sort.py

--- Inflearn_coding_test/3주차/01_04_merge_sort.py
+++ Inflearn_coding_test/3주차/01_04_merge_sort.py
@@ -21,12 +21,12 @@ def merge(array1, array2):
     while array2_index < len(array2):
         result.append(array2[array2_index])
         array2_index += 1
-    # 이 부분을 채워보세요!
+   
 
     return result
 
 
-print(merge(array_a, array_b))  # [1, 2, 3, 4, 5, 6, 7, 8] 가 되어야 합니다!
+print(merge(array_a, array_b))  # [1, 2, 3, 4, 5, 6, 7, 8] 가 되어야 합니다.
 
 print("정답 = [-7, -1, 5, 6, 9, 10, 11, 40] / 현재 풀이 값 = ", merge([-7, -1, 9, 40], [5, 6, 10, 11]))
 print("정답 = [-1, 2, 3, 5, 10, 40, 78, 100] / 현재 풀이 값 = ", merge([-1,2,3,5,40], [10,78,100]))
